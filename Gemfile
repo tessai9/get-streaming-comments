42923fd07eadaeeca2a5cfc8a87349546e541edc
Tesao <shimeji9@gmail.com>
2021-05-28 00:15:58 +0900

    export to csv

--- Gemfile
+++ Gemfile
@@ -4,8 +4,6 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-# gem "rails"
-
 gem 'dotenv', '~> 2.7', '>= 2.7.6'
 gem 'string-similarity', '~> 2.1'
 gem 'faraday', '~> 1.1'
--- main.rb
+++ main.rb
@@ -4,39 +4,37 @@ require "json"
 require "dotenv"
 require "csv"
 
+# Raise error if no argument provided
+raise ArgumentError, "Execute this script with stream ID" if ARGV.empty?
+
 # load environment values
 Dotenv.load
 
-# Request URI
+# Consts
 YOUTUBE_API_URI = 'https://www.googleapis.com/youtube/v3'.freeze
 API_KEY = ENV['API_KEY'].freeze
-
-if ARGV[0].nil?
-  puts "error: input stream ID"
-  exit
-end
-
-# Get video id from argument
-video_id = ARGV[0]
-
-paths = {
+API_PATH = {
   :streaming => "/liveChat/messages",
   :videos => "/videos"
-}
+}.freeze
 
+# Get video id from argument
+video_id = ARGV.first
+
+# Set request info
 params_for_videos = {
   :part => "liveStreamingDetails",
   :id => video_id,
   :key => API_KEY
 }
 
-# puts "Target livestreaming is : " + video_id
-
 # Get channel ID from the streaming
-resp_from_videos = Faraday.get(YOUTUBE_API_URI + paths[:videos], params_for_videos)
-chat_id = JSON.parse(resp_from_videos.body)["items"][0]["liveStreamingDetails"]["activeLiveChatId"]
+resp_from_videos = Faraday.get("#{YOUTUBE_API_URI}#{API_PATH[:videos]}", params_for_videos)
+chat_id = JSON.parse(resp_from_videos.body, symbolize_names: true)[:items]
+            .first[:liveStreamingDetails][:activeLiveChatId]
 
-# puts "Active Chat ID is : " + chat_id
+# Create file
+f = File.new("./tmp/comments_#{video_id}_#{Time.now.strftime('%Y%m%d_%H%M%S')}.csv", "a")
 
 # Get comments from the streaming
 pageToken = 1
@@ -48,31 +46,38 @@ params_for_streaming = {
   :liveChatId => chat_id,
   :key => API_KEY,
 }
+
 while offlinedAt.nil?
-  comments = []
+  output = []
   begin
 
-    resp_from_liveChat = Faraday.get(YOUTUBE_API_URI + paths[:streaming], params_for_streaming)
+    # Get data from streaming
+    resp_from_liveChat = Faraday.get("#{YOUTUBE_API_URI}#{API_PATH[:streaming]}", params_for_streaming)
 
-    res_body = JSON.parse(resp_from_liveChat.body)
-    res_body['items'].each do |chat|
-      comments << chat['snippet']['authorChannelId']
-      comments << chat['snippet']['displayMessage']
+    res_body = JSON.parse(resp_from_liveChat.body, symbolize_names: true)
+    comments = res_body[:items]
+    comments.each do |chat|
+      snippet = chat[:snippet]
+      row = CSV.generate_line([
+                          snippet[:publishedAt],
+                          snippet[:authorChannelId],
+                          snippet[:displayMessage],
+                        ])
+      f.puts row
     end
-    puts comments
 
-    params_for_streaming[:nextPageToken] = res_body['nextPageToken']
+    params_for_streaming[:nextPageToken] = res_body[:nextPageToken]
 
-    sleep res_body['pollingIntervalMillis'] / 1000
+    sleep res_body[:pollingIntervalMillis] / 1000
   rescue Interrupt
+    f.close
     puts "bye"
     break
   rescue
+    f.close
     puts "Program exited by unexpected error"
     break
   end
 end
 
-# Create CSV file
-# CSV.open(video_id + ".csv", "w") do |csv|
-# end
+f.close
